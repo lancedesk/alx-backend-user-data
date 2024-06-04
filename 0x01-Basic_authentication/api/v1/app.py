ea5db612e915a39bdbd292d07fdc50bb96c1ad6a
Lance Desk <lncedesk@gmail.com>
2024-06-04 10:45:24 -0700

    Error handler: Unauthorized.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,22 +4,26 @@ Route module for the API
 """
 import os
 from os import getenv
-from flask_cors import (CORS, cross_origin)
-from flask import Flask, jsonify, abort, request
 
-from api.v1.auth.auth import Auth
 from api.v1.views import app_views
-from api.v1.auth.basic_auth import BasicAuth
+from flask_cors import CORS, cross_origin
+from flask import Flask, abort, jsonify, request
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
+
+"""
+Check AUTH_TYPE
+"""
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
-if auth_type == 'basic_auth':
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
@@ -52,18 +56,20 @@ def before_request():
     """
     Handle user authentication before resource access
     """
-    if auth:
-        paths = ['/api/v1/status/',
-                 '/api/v1/unauthorized/',
-                 '/api/v1/forbidden/']
-
-        if auth.require_auth(request.path, paths):
-            auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
-            if user is None:
-                abort(403)
+    if auth is None:
+        return
+    if auth.require_auth(
+            path=request.path,
+            exclude_paths=[
+                '/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/',
+            ],
+    ):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
