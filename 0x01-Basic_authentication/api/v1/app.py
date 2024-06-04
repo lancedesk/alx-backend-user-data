cfb3f9dfc9b644cbbed2a2c349ddcb3a766c1eb0
Lance Desk <lncedesk@gmail.com>
2024-06-04 10:31:10 -0700

    Error handler: Unauthorized.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,25 +4,31 @@ Route module for the API
 """
 import os
 from os import getenv
+from flask_cors import (CORS, cross_origin)
+from flask import Flask, jsonify, abort, request
+
 from api.v1.auth.auth import Auth
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
-
 if auth_type == 'auth':
     auth = Auth()
-
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """
+    Handle not found
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
@@ -39,14 +45,6 @@ def forbidden(error) -> str:
     Handle forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """
-    Handle not found
-    """
-    return jsonify({"error": "Not found"}), 404
 
 
 @app.before_request
