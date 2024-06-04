2b7fac24affb73af05bd0e5fafd9f41422c0c9f1
Lance Desk <lncedesk@gmail.com>
2024-06-04 10:23:07 -0700

    Error handler: Unauthorized.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,11 +4,12 @@ Route module for the API
 """
 import os
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
 from api.v1.auth.basic_auth import BasicAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
