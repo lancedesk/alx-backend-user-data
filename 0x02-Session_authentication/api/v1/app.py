2ef94bf82e270c02a20ac124ee45859691f5e8f7
Lance Desk <lncedesk@gmail.com>
2024-06-06 12:36:39 -0700

    A class SessionAuth that inherits from Auth.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,64 +2,40 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-from api.v1.views import app_views
 
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 """
 Check AUTH_TYPE
 """
-auth = None
-auth_type = getenv('AUTH_TYPE')
-
-if auth_type == 'auth':
+if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-if auth_type == 'basic_auth':
+elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-if auth_type == 'session_auth':
+elif getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
-if auth_type == 'session_exp_auth':
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 
-if auth_type == 'session_db_auth':
+elif getenv('AUTH_TYPE') == 'session_db_auth':
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
-
-
-@app.before_request
-def before_request():
-    """
-    Handle user authentication before resource access
-    """
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if auth is None:
-        return
-    if auth.require_auth(request.path, excluded_paths) is False:
-        return
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
-        abort(401)
-
-    current_user = auth.current_user(request)
-    print(current_user)
-    if current_user is None:
-        abort(403)
-
-    request.current_user = current_user
 
 
 @app.errorhandler(404)
@@ -84,6 +60,30 @@ def forbidden(error) -> str:
     Handle forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> str:
+    """
+    Handle user authentication before resource access
+    """
+    if auth is None:
+        return
+    if auth.require_auth(
+            path=request.path,
+            exclude_paths=[
+                '/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/',
+            ],
+    ):
+        if auth.authorization_header(request) is None and auth.session_cookie(
+                request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
